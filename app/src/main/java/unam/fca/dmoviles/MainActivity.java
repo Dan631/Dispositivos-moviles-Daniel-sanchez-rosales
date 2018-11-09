96d438c118df2bff4b5b889699fab6c618c00732
Dan631 <42328545+Dan631@users.noreply.github.com>
2018-11-08 18:29:41 -0600

    Se agrego función para calculo de porcentaje ademas de soporte para rotar pantalla sin perder los datos

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,14 +2,20 @@ package unam.fca.dmoviles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int IMAGE_WIDTH = 400;
+    private final static String TEXT_VIEW_KEY = "TEXT_VIEW_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         //Actividad 8 Creacion de una calculadora de porcentajes
         //En esta actividad crearas una aplicacion que calcule la diferencia relativa en porcentaje entre dos numeros.
@@ -19,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
+        final TextView textViewResult = findViewById(R.id.textViewResult);
+        final Button calcular = findViewById(R.id.calcular);
+        final EditText numero1 = (EditText)findViewById(R.id.editText);
+        final EditText numero2 = (EditText)findViewById(R.id.editText2);
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Float num1 = Float.parseFloat(numero1.getText().toString());
+                Float num2 = Float.parseFloat(numero2.getText().toString());
+                Float total=((num2-num1)/num1)*100;
+                textViewResult.setText(total.toString()+'%');
+            }
+        });
 
 
         //Actividades
@@ -34,4 +53,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        final TextView textViewResult = findViewById(R.id.textViewResult);
+        final EditText numero1 = findViewById(R.id.editText);
+        final EditText numero2 = findViewById(R.id.editText2);
+        String e1 = numero1.getText().toString();
+        String e2 = numero2.getText().toString();
+        outState.putString("numero1", e1);
+        outState.putString("numero2", e2);
+        outState.putString(TEXT_VIEW_KEY, textViewResult.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        final TextView textViewResult = findViewById(R.id.textViewResult);
+        final EditText editText = findViewById(R.id.editText);
+        final EditText editText2 = findViewById(R.id.editText2);
+        String e1 = savedInstanceState.getString("numero1");
+        String e2 = savedInstanceState.getString("numero2");
+        textViewResult.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+        editText.setText(e1);
+        editText2.setText(e2);
+    }
+
+
 }
